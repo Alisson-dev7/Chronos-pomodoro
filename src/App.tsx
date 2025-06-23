cf60228231e7c073400f68ed58b5a9aebc8fbd93
Alisson <josealisson977@gmail.com>
2025-06-23 14:31:28 -0300

    3 parte

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,46 @@
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+
 import './Styles/theme.css';
 import './Styles/global.css';
-import { Container } from './components/Container/Container';
+import { CountDown } from './components/CountDown';
+import { Input } from './components/Input';
 
 export function App() {
   return (
     <>
       <Container>
-        <section>Logo</section>
+        <Logo></Logo>
       </Container>
+
       <Container>
-        <section>Menu</section>
+        <Menu></Menu>
+      </Container>
+
+      <Container>
+        <CountDown></CountDown>
+      </Container>
+
+      <Container>
+        <form className='form'>
+          <div className='formRow'>
+            <Input id='input' type='text' labelText='task' placeholder='Digite Algo'></Input>
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0</p>
+          </div>
+
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
