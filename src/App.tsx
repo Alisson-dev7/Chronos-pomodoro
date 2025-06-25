ae237d88cb4af80ed2cced7955f85447a2ee9563
Alisson <josealisson977@gmail.com>
2025-06-25 14:01:22 -0300

    Aula 36

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
-
-import './Styles/theme.css';
-import './Styles/global.css';
 import { CountDown } from './components/CountDown';
 import { Input } from './components/Input';
 import { Cycles } from './components/Cycles';
 import { DefaultButton } from './components/DefaultButton';
 import { PlayCircleIcon } from 'lucide-react';
+import { Footer } from './components/Footer';
+
+import './Styles/theme.css';
+import './Styles/global.css';
 
 export function App() {
   return (
@@ -28,7 +29,12 @@ export function App() {
       <Container>
         <form className='form'>
           <div className='formRow'>
-            <Input id='input' type='text' labelText='task' placeholder='Digite Algo'></Input>
+            <Input
+              id='input'
+              type='text'
+              labelText='task'
+              placeholder='Digite Algo'
+            ></Input>
           </div>
 
           <div className='formRow'>
@@ -36,13 +42,17 @@ export function App() {
           </div>
 
           <div className='formRow'>
-            <Cycles/>
+            <Cycles />
           </div>
 
           <div className='formRow'>
-            <DefaultButton icon={<PlayCircleIcon/>}/>
+            <DefaultButton icon={<PlayCircleIcon />} />
           </div>
         </form>
+      </Container>
+
+      <Container>
+        <Footer></Footer>
       </Container>
     </>
   );
--- src/components/DefaultButton/index.tsx
+++ src/components/DefaultButton/index.tsx
@@ -1,12 +1,13 @@
 import styles from './styles.module.css';
 type ButtonProps = {
   icon: React.ReactNode;
+  color?: 'green' | 'red'
 } & React.ComponentProps<'button'>;
 
-export function DefaultButton({icon, ...props }: ButtonProps) {
+export function DefaultButton({icon, color = 'green',...props }: ButtonProps) {
   return (
     <>
-      <button className={styles.button} {...props}>
+      <button className={`${styles.button} ${styles[color]}`} {...props}>
           {icon}
       </button>
     </>
